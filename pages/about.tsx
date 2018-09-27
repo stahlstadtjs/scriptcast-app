6662f8d3a3ca15254aefc2e28568275ca4f4d0d6
Stefan Baumgartner <sbaumg@gmail.com>
2018-09-27 22:56:40 +0200

    add title

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,10 @@
 import ArticleLayout from '../components/ArticleLayout';
 import About from '../articles/about.mdx';
+import Head from 'next/head';
 
-export default () => <>
-  <ArticleLayout>
-    <About />
-  </ArticleLayout>
-</>;
+export default () => <ArticleLayout>
+  <Head>
+    <title>About ScriptCast</title>
+  </Head>
+  <About />
+</ArticleLayout>;
